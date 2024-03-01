a234276b25479fb53dcf0ebb13b8e064f36b6244
UshiromiyaGandhi <gandhidhuta123123@gmail.com>
2024-03-01 17:50:50 +0700

    sign up page dibenerin

--- backend/assets/AppAsset.php
+++ backend/assets/AppAsset.php
@@ -3,21 +3,31 @@
 namespace backend\assets;
 
 use yii\web\AssetBundle;
+use yii\web\View;
 
 /**
  * Main backend application asset bundle.
  */
 class AppAsset extends AssetBundle
 {
-    public $basePath = '@webroot';
-    public $baseUrl = '@web';
-    public $css = [
-        'css/site.css',
-    ];
-    public $js = [
-    ];
-    public $depends = [
-        'yii\web\YiiAsset',
-        'yii\bootstrap5\BootstrapAsset',
-    ];
+	public $depends = [
+//		'yii\web\YiiAsset',
+//		'yii\bootstrap5\BootstrapAsset',
+	];
+	public $basePath = '@webroot';
+	public $baseUrl = '@web';
+	public $css = [
+		'css/tw.css',
+		'template1font.css',
+	];
+	public $js = [
+		'js/index.js',
+		'js/us-aea-en.js',
+		'js/components/chart-01.js',
+		'js/components/chart-02.js',
+		'js/components/chart-03.js',
+		'js/components/chart-04.js',
+		'js/components/map-01.js',
+		['https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js', ['position' => View::POS_END, 'defer' => true, 'async' => true]]
+	];
 }
