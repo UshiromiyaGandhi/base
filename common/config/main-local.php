5b2a53a2419f907929e9ea01b194c3659370e3b8
UshiromiyaGandhi <gandhidhuta123123@gmail.com>
2024-04-01 17:44:40 +0700

    database name change

--- common/config/main-local.php
+++ common/config/main-local.php
@@ -4,7 +4,7 @@ return [
     'components' => [
         'db' => [
             'class' => \yii\db\Connection::class,
-            'dsn' => 'mysql:host=localhost;dbname=base4',
+            'dsn' => 'mysql:host=localhost;dbname=',
             'username' => 'root',
             'password' => '',
             'charset' => 'utf8',
